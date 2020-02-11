0f84e939c2ef5d0f7f2647c41d4b358bc12013f1
Quentin Grimonprez <quentin.grimonprez@inria.fr>
2020-02-11 14:54:04 +0100

    beautify code

--- R/Terminator.R
+++ R/Terminator.R
@@ -1,8 +1,8 @@
 #' Destructing values to have missing ones
 #' 
-#' @param wrath the ratio of missing values in the output
 #' @param target the dataset (matrix or data.frame) in which missing values will be made
-#' @param diag if =1 it creates a diagonal band of missing values (no complete line, no complete column, but not too much missing values)
+#' @param wrath the ratio of missing values in the output
+#' @param diag if == 1 it creates a diagonal band of missing values (no complete line, no complete column, but not too much missing values)
 #' @param Z adjacency matrix to coerce a maximum of 1 missing value per sub-regression for each individual
 #' 
 #' @return the matrix with missing values.
@@ -16,7 +16,7 @@
 #' 
 #' # create a diagonal of missing values
 #' datamiss <- Terminator(target = data, diag = 1)
-#' showdata(datamiss) #plot positions of the missing values (no full individuals, no full variable)
+#' showdata(datamiss) # plot positions of the missing values (no full individuals, no full variable)
 #'    
 #' @export 
 Terminator <- function(target = NULL, wrath = 0.1, diag = 0, Z = NULL){
@@ -34,7 +34,7 @@ Terminator <- function(target = NULL, wrath = 0.1, diag = 0, Z = NULL){
       n <- nrow(target)
       p <- ncol(target)
       
-      quidiag <- cbind(rep(1:n, length.out = max(n, p)), rep(1:p, length.out = max(n,p)))
+      quidiag <- cbind(rep(1:n, length.out = max(n, p)), rep(1:p, length.out = max(n, p)))
       target[quidiag] = NA
       
       for(j in 2:diag){
@@ -47,7 +47,7 @@ Terminator <- function(target = NULL, wrath = 0.1, diag = 0, Z = NULL){
       n <- nrow(target)
       p <- ncol(target)
       
-      quidiag <- cbind(rep(1:n, length.out = max(n, p)), rep(1:p, length.out = max(n,p)))
+      quidiag <- cbind(rep(1:n, length.out = max(n, p)), rep(1:p, length.out = max(n, p)))
       nbmank <- floor(wrath*n*p)
       loc <- target
       target <- NA * target
@@ -62,16 +62,16 @@ Terminator <- function(target = NULL, wrath = 0.1, diag = 0, Z = NULL){
             for(j in 1:length(quiZ)){ # maxi pr manquants
                if(length(quiZ) > 0){
                   mankloc = sample(quiZ, size = 1) # on tue quelqu'un
-                  quiZ = quiZ[quiZ!=mankloc] # le mort n'est plus candidat ni bloquable
-                  if(Zc[mankloc]>0){ # variable a gauche, on retire la regression
-                     reste = which(Z[,mankloc]!=0)
+                  quiZ = quiZ[quiZ != mankloc] # le mort n'est plus candidat ni bloquable
+                  if(Zc[mankloc] > 0){ # variable a gauche, on retire la regression
+                     reste = which(Z[, mankloc] != 0)
                      quiblok = c(quiblok, reste)
                      quiZ = unique(c(reste, quiZ))[-c(1:length(reste))] # on enleve les bloques
                   }else{ # variable a droite, on retire la gauche, les autres a droites, et les autres regressions touchees
                      impact = quiZ_vect[quiZ_vect[1,] == mankloc, 2]
                      for(k in impact){
                         if(length(quiZ) > 0){
-                           ssreg = c(k,which(Z[,k] != 0))
+                           ssreg = c(k, which(Z[,k] != 0))
                            ssreg = ssreg[ssreg != mankloc]
                            quiblok = unique(c(quiblok, ssreg))
                            quiZ = unique(c(quiblok, quiZ))[-c(1:length(quiblok))] # on enleve les bloques
