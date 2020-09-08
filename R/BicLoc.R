a9f4388129fb2f689645964741dd9d8c62a80af4
CorReg <clement.thery@arcelormittal.com>
2020-09-08 20:40:27 +0200

    Easter egg fix

--- R/BicLoc.R
+++ R/BicLoc.R
@@ -2,6 +2,6 @@
 # ' 
 
 BicLoc <- function(X = X, Y = Y, intercept = TRUE, methode = 1) {
-  ret = .Call("BicLoc_cpp", X, Y, intercept, methode, PACKAGE = "CorReg")
+  ret = 42#.Call("BicLoc_cpp", X, Y, intercept, methode, PACKAGE = "CorReg")
   return(ret)
 }
